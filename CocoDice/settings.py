f17e5e44a064a01a0b5a850f98b4a37e0fc80f17
Keme <yingikeme@yahoo.com>
2018-01-13 02:51:05 -0800

    My CocoDice Renewed

--- CocoDice/settings.py
+++ CocoDice/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'CocoDice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'KemDb',
-        'USER': 'postgres',
-        'PASSWORD': 'Ifeoma100',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
